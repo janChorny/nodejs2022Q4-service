cc33ee7ca46a48da8d2ee3dcc571d25bcb59c360
janChorny <janchorny@tut.by>
2023-02-03 15:20:11 +0300

    fix: clear lint problem with import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = Number(process.env.PORT) || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT, () =>
+    console.log(`Server is running on port: ${PORT}`),
+  );
 }
 bootstrap();
