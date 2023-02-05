1f35ba03a43e1eb87c6408a9cd2888309d2c9722
janChorny <janchorny@tut.by>
2023-02-05 13:14:44 +0300

    fix: clear wrong dto problem with testing tracks

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -11,6 +11,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { validate, version } from 'uuid';
 import { CreateTrackDTO } from './dto/trackCreate.dto';
 import { UpdateTrackDTO } from './dto/trackUpdate.dto';
 import { TrackService } from './track.service';
@@ -50,6 +51,18 @@ export class TrackController {
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateTrackDTO: UpdateTrackDTO,
   ) {
+    const { name, artistId, albumId, duration } = updateTrackDTO;
+    if (
+      typeof name !== 'string' ||
+      typeof duration !== 'number' ||
+      (validate(artistId) && version(artistId) === 4) ||
+      (validate(albumId) && version(albumId))
+    ) {
+      throw new HttpException(
+        `Not all the provided fields are valid`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     const track = this.trackService.getTrack(id);
     if (!track) {
       throw new HttpException(
