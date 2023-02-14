7f6f5ee958be8e472077ae1a6bf32a640fad0df2
janChorny <janchorny@tut.by>
2023-02-15 00:42:36 +0300

    fix: clarify env database and ormconfig

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -66,4 +66,74 @@ export class UserController {
   deleteUser(@Param('userId', new ParseUUIDPipe()) userId: string) {
     return this.userService.delete(userId);
   }
+
+  // @HttpCode(HttpStatus.OK)
+  // @Get()
+  // getAllUsers() {
+  //   return this.userService.getAllUsers();
+  // }
+
+  // @HttpCode(HttpStatus.OK)
+  // @Get(':id')
+  // getUser(@Param('id', new ParseUUIDPipe()) id: string) {
+  //   const user = this.userService.getUser(id);
+  //   if (!user) {
+  //     throw new HttpException(
+  //       `User with such id is not found`,
+  //       HttpStatus.NOT_FOUND,
+  //     );
+  //   }
+  //   return user;
+  // }
+
+  // @HttpCode(HttpStatus.CREATED)
+  // @Post()
+  // createUser(@Body() createUserDTO: CreateUserDTO) {
+  //   return this.userService.createUser(createUserDTO);
+  // }
+
+  // @HttpCode(HttpStatus.OK)
+  // @Put(':id')
+  // updateUserPassword(
+  //   @Param('id', new ParseUUIDPipe()) id: string,
+  //   @Body() updateUserPasswordDTO: UpdatePasswordDTO,
+  // ) {
+  //   const { oldPassword, newPassword } = updateUserPasswordDTO;
+  //   if (!oldPassword && !newPassword) {
+  //     throw new HttpException(
+  //       `Not all the required fields are provided`,
+  //       HttpStatus.BAD_REQUEST,
+  //     );
+  //   }
+
+  //   const user = this.userService.getUser(id);
+  //   if (!user) {
+  //     throw new HttpException(
+  //       `User with such id is not found`,
+  //       HttpStatus.NOT_FOUND,
+  //     );
+  //   }
+
+  //   if (user.password !== oldPassword) {
+  //     throw new HttpException(
+  //       `Old user's password is wrong`,
+  //       HttpStatus.FORBIDDEN,
+  //     );
+  //   }
+
+  //   return this.userService.updateUserPassword(id, updateUserPasswordDTO);
+  // }
+
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  // @Delete(':id')
+  // deleteUser(@Param('id', new ParseUUIDPipe()) id: string) {
+  //   const user = this.userService.getUser(id);
+  //   if (!user) {
+  //     throw new HttpException(
+  //       `User with such id is not found`,
+  //       HttpStatus.NOT_FOUND,
+  //     );
+  //   }
+  //   return this.userService.deleteUser(id);
+  // }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { dataBase } from 'src/constants/constants';
-import { v4 } from 'uuid';
 import { CreateUserDTO } from './dto/userCreate.dto';
-import { UpdatePasswordDTO } from './dto/userUpdate.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
@@ -73,49 +70,49 @@ export class UserService {
     }
   }
 
-  getAllUsers() {
-    return dataBase.users;
-  }
+  // getAllUsers() {
+  //   return dataBase.users;
+  // }
 
-  getUser(id: string) {
-    const user = dataBase.users.find((user) => user.id === id);
-    return user;
-  }
+  // getUser(id: string) {
+  //   const user = dataBase.users.find((user) => user.id === id);
+  //   return user;
+  // }
 
-  createUser(createUserDTO: CreateUserDTO) {
-    const user = {
-      id: v4(),
-      login: createUserDTO.login,
-      password: createUserDTO.password,
-      version: 1,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    };
+  // createUser(createUserDTO: CreateUserDTO) {
+  //   const user = {
+  //     id: v4(),
+  //     login: createUserDTO.login,
+  //     password: createUserDTO.password,
+  //     version: 1,
+  //     createdAt: Date.now(),
+  //     updatedAt: Date.now(),
+  //   };
 
-    dataBase.users.push(user);
-    const result = { ...user };
-    delete result.password;
-    return result;
-  }
+  //   dataBase.users.push(user);
+  //   const result = { ...user };
+  //   delete result.password;
+  //   return result;
+  // }
 
-  updateUserPassword(id: string, updateUserPasswordDTO: UpdatePasswordDTO) {
-    const userToUpdateIndex = dataBase.users.findIndex(
-      (user) => user.id === id,
-    );
-    const userToUpdate = dataBase.users[userToUpdateIndex];
-    dataBase.users[userToUpdateIndex] = {
-      ...userToUpdate,
-      password: updateUserPasswordDTO['newPassword'],
-      version: userToUpdate.version + 1,
-      updatedAt: Date.now(),
-    };
-    const result = { ...dataBase.users[userToUpdateIndex] };
-    delete result.password;
-    return result;
-  }
+  // updateUserPassword(id: string, updateUserPasswordDTO: UpdatePasswordDTO) {
+  //   const userToUpdateIndex = dataBase.users.findIndex(
+  //     (user) => user.id === id,
+  //   );
+  //   const userToUpdate = dataBase.users[userToUpdateIndex];
+  //   dataBase.users[userToUpdateIndex] = {
+  //     ...userToUpdate,
+  //     password: updateUserPasswordDTO['newPassword'],
+  //     version: userToUpdate.version + 1,
+  //     updatedAt: Date.now(),
+  //   };
+  //   const result = { ...dataBase.users[userToUpdateIndex] };
+  //   delete result.password;
+  //   return result;
+  // }
 
-  deleteUser(id: string) {
-    dataBase.users = dataBase.users.filter((user) => user.id !== id);
-    return;
-  }
+  // deleteUser(id: string) {
+  //   dataBase.users = dataBase.users.filter((user) => user.id !== id);
+  //   return;
+  // }
 }
